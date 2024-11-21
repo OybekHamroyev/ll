3e72328ae72aa45c98d4e3b3875097baf32adceb
Oybek <aquarius280186@gmail.com>
2024-11-21 23:56:24 +0500

    salom yoz

--- src/main/java/com/example/testing47/Testing47Application.java
+++ src/main/java/com/example/testing47/Testing47Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Testing47Application {
 
     public static void main(String[] args) {
+        //salom
         SpringApplication.run(Testing47Application.class, args);
     }
 
